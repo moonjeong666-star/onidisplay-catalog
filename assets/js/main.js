8c50569a09574ef94d15a728f7fc12435da9a448
394192392 <moonjeong666@gmail.com>
2026-08-27 15:45:27 +0800

    增加

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,8 @@ const catalogs=[
 ["What to prepare for Store Design & Fixture Production","assets/pdf/What_to_prepare_for_Store_Design&Fixture_Production.pdf","Store Design & Fixture Production.webp"],
 ["What ONI Do for Retailers 2026","assets/pdf/What_ONI_Do_for_Retailers_2026.pdf","What ONI Do for Retailers 2026.webp"],
 ["Guangzhou ONI Shop Design Price List","assets/pdf/Guangzhou_ONI_Shop_Design_Price_List.pdf","Shop Design.webp"],
-["ONI Commercial Space Design Pricing Guide","assets/pdf/ONI_Commercial_Space_Design_Pricing_Guide.pdf","ONI Commercial Space Design Pricing Guide.webp"]
+["ONI Commercial Space Design Pricing Guide","assets/pdf/ONI_Commercial_Space_Design_Pricing_Guide.pdf","ONI Commercial Space Design Pricing Guide.webp"],
+["ONI Final Drawing and Material Approval Notice","assets/pdf/ONI_Final_Drawing_and_Material_Approval_Notice.pdf","ONI_Final_Drawing_and_Material_Approval_Notice.webp"]
 ];
 
 const grid=document.querySelector(".catalog-grid");
